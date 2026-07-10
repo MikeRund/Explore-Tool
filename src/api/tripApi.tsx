6b947b9ae82b308e0982f0166dcf0f49f44de906
Mike Rundle <129297573+MikeRund@users.noreply.github.com>
2026-07-10 10:56:15 +0100

    created mock api and api-utils

---
+++ src/api/tripApi.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export interface Trip {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  itineraryItems: string[];
+  packingList: string[];
+  image: string;
+}
+
+const API_BASE_URL = "http://localhost:3000/trips";
+
+const fetchTrips = async (): Promise<Trip[]> => {
+  const response = await axios.get<Trip[]>(API_BASE_URL);
+  return response.data;
+};
+
+const getTrip = async (id: string): Promise<Trip> => {
+  const response = await axios.get<Trip>(`${API_BASE_URL}/${id}`);
+  return response.data;
+};
+
+const postTrip = async (trip: Trip): Promise<Trip> => {
+  const response = await axios.post<Trip>(API_BASE_URL, trip);
+  return response.data;
+};
+
+const updateTrip = async (id: string, trip: Trip): Promise<Trip> => {
+  const response = await axios.put<Trip>(`${API_BASE_URL}/${id}`, trip);
+  return response.data;
+};
+
+const deleteTrip = async (id: string): Promise<void> => {
+  await axios.delete(`${API_BASE_URL}/${id}`);
+};
+export { fetchTrips, getTrip, postTrip, updateTrip, deleteTrip, Trip };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,12 +8,46 @@ import TripCard from "../components/TripCard";
 import "../styles/HomePage.css";
 import { useState } from "react";
 import AddTripModal from "./AddTripModal";
+import {
+  deleteTrip,
+  fetchTrips,
+  getTrip,
+  updateTrip,
+  type Trip,
+} from "../api/tripApi";
 
 export default function HomePage() {
   const [showModal, setShowModal] = useState(false);
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
+  const handleDelete = async () => {
+    // Placeholder for fetching trips from an API or database
+    console.log("Deleting trip by id...");
+    await deleteTrip("2"); // Assuming getTrip is a function that fetches a trip by id
+    console.log("Trip deleted.");
+  };
+  const handleOnClick = async () => {
+    // Placeholder for fetching trips from an API or database
+    console.log("Fetching trips...");
+    const trips = await fetchTrips();
+    console.log(trips); // Assuming getTrip is a function that fetches a trip by id
+  };
+  const handleUpdateTrip = async () => {
+    // Placeholder for updating a trip in an API or database
+    console.log("Updating trip...");
+    const trip: Trip = {
+      id: "kS0xfcrf2fs",
+      title: "Surfing in Portugal - Updated",
+      description: "A surfing adventure in Portugal",
+      date: "2024-07-20",
+      itineraryItems: [],
+      packingList: [],
+      image: "surf.jpg",
+    };
 
+    const updatedTrip = await updateTrip(trip.id, trip);
+    console.log(updatedTrip); // Assuming getTrip is a function that fetches a trip by id
+  };
   return (
     <div className="container-fluid p-0">
       <HeaderImage imageUrl={surfBanner}>
@@ -24,15 +58,17 @@ export default function HomePage() {
         <div className="d-flex justify-content-between align-items-center mb-3">
           <Title />
           <div className="d-flex gap-4">
+            <button className="button-56" onClick={handleOnClick}>
+              Get Trips
+            </button>
             <button className="button-56" onClick={handleShowModal}>
               Add Trip
             </button>
             <AddTripModal show={showModal} handleClose={handleCloseModal} />
-            <button
-              className="button-56"
-              role="button"
-              onClick={() => alert("Delete Trip button clicked!")}
-            >
+            <button className="button-56" onClick={handleUpdateTrip}>
+              Update Trip
+            </button>
+            <button className="button-56" role="button" onClick={handleDelete}>
               Delete Trip
             </button>
           </div>
